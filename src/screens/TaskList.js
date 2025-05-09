2fccc0a960a254a618b001d9566c738fe12b9436
brunalopes1996 <bruuna.oliveiraa1@gmail.com>
2025-05-08 21:57:48 -0300

    funcionando o botão salvar

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -94,7 +94,12 @@ export default function TaskList() {
 
     return (
         <View style={styles.container}>
-            <AddTask isVisible={showAddTask} onCancel={() => setShowAddTask(false)}/>
+
+            <AddTask isVisible={showAddTask} 
+                onCancel={() => setShowAddTask(false)}
+                onSave={addTask}
+                />
+            
             <ImageBackground source={todayImage} style={styles.background}>
                 <View style={styles.iconBar}>
                     <TouchableOpacity onPress={toggleFilter}>
