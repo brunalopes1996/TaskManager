bb26ef81eaaa5609c79f51d4e4ab758629ce49f6
brunalopes1996 <bruuna.oliveiraa1@gmail.com>
2025-05-13 22:26:40 -0300

    implementa a funcionalidade de exclusão de tarefa

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -92,6 +92,11 @@ export default function TaskList() {
         setShowAddTask(false)
     }
 
+    const deleteTask = id => {
+        const tempTasks = tasks.filter(task => task.id !== id)
+        setTasks(tempTasks)
+    }
+
     return (
         <View style={styles.container}>
 
@@ -117,7 +122,8 @@ export default function TaskList() {
                 <FlatList
                     data={visibleTasks}
                     keyExtractor={item => `${item.id}`}
-                    renderItem={({ item }) => <Task {...item} onToggleTask={toggleTask} />}
+                    renderItem={({ item }) => 
+                        <Task {...item} onToggleTask={toggleTask} onDelete={deleteTask} />}
                 />
             </View>
 
